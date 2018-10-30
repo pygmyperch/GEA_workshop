2df8e11f4d4294a43395be17b7b556399a97fdec
cb <pygmyperch@gmail.com>
2018-10-30 16:56:20 +1030

    update

---
+++ R/example.env.R
@@ -0,0 +1,7 @@
+#' Environmental data for GEA workshop
+#'
+#' @examples
+#' data(example.env)
+#'
+#' @format A table with location names, x and y coordinates in decimal degrees and several environmental variables
+"example.env"
--- R/rainbow.env.R
+++
@@ -1,7 +0,0 @@
-#' Environmental data for Melanotaenia fluviatilis sampling sites in the Murray-Darling Basin
-#'
-#' @examples
-#' data(rainbow.env)
-#'
-#' @format A table with location names, x and y coordinates in decimal degrees and six PCA axes describing hydroclimatic variation
-"rainbow.env"
