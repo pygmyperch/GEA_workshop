46e78886993c30bad6adc9f30e27c90d3c9bac8b
cb <pygmyperch@gmail.com>
2018-10-30 16:48:55 +1030

    added data desc.

---
+++ R/rainbow.env.R
@@ -0,0 +1,7 @@
+#' Environmental data for Melanotaenia fluviatilis sampling sites in the Murray-Darling Basin
+#'
+#' @examples
+#' data(rainbow.env)
+#'
+#' @format A table with location names, x and y coordinates in decimal degrees and six PCA axes describing hydroclimatic variation
+"rainbow.env"
